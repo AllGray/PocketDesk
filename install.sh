b71321fbb6df1edf0b96275485fb436b1e29eab6
AllGray <mathiasthur@gmail.com>
2017-07-14 23:13:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,19 @@ fi
 # Clear the screen
 reset
 
-# Start info
-
+# install dependencies
 apt -y upgrade
 apt -y install git xinput-calibrator
 
+# Touchscreen
 mv /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
 wget -O /etc/X11/xorg.conf https://raw.githubusercontent.com/AllGray/PocketDesk/master/xorg.conf
 
+# Keyboard
 wget -O /home/chip/.Xmodmap https://raw.githubusercontent.com/AllGray/PocketDesk/master/.Xmodmap
-
 wget -O /home/chip/.xinitrc https://raw.githubusercontent.com/AllGray/PocketDesk/master/.xinitrc
 
-
+# Battery
 git clone https://github.com/editkid/chip-battery-status.git
 cd chip-battery-status
 sudo ./install.sh
