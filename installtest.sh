860408bc742f7ef93b2422a177382456ce124658
AllGray <mathiasthur@gmail.com>
2017-09-03 19:50:38 +0200

    Update installtest.sh

--- installtest.sh
+++ installtest.sh
@@ -59,4 +59,4 @@ sed -i -e 's#surf /usr/share/pocketchip-localdoc/index.html#pkill -KILL -u chip#
 
 # Wallpaper
 wget https://raw.githubusercontent.com/AllGray/PocketDesk/master/PocketDESK_wall.png /home/chip/.pocket-home/PocketDESK_wall.png
-sed -i -e 's/"background": "4D4D4D",/"background": "/home/chip/.pocket-home/PocketDESK_wall.png",/g'  ~/.pocket-home/config.json
+sed -i -e 's#"background": "4D4D4D",#"background": "/home/chip/.pocket-home/PocketDESK_wall.png",#g'  ~/.pocket-home/config.json
