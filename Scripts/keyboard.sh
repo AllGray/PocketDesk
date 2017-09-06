dd205cfde0f7b849ff8863a3e90278a0a5dce4ee
AllGray <mathiasthur@gmail.com>
2017-09-06 15:18:37 +0200

    Update keyboard.sh

--- Scripts/keyboard.sh
+++ Scripts/keyboard.sh
@@ -1,3 +1,3 @@
 # Keyboard
-wget -O /home/chip/.Xmodmap https://raw.githubusercontent.com/AllGray/PocketDesk/master/.Xmodmap
-wget -O /home/chip/.xinitrc https://raw.githubusercontent.com/AllGray/PocketDesk/master/.xinitrc
+wget -O /home/chip/.Xmodmap https://raw.githubusercontent.com/AllGray/PocketDesk/master/files/.Xmodmap
+wget -O /home/chip/.xinitrc https://raw.githubusercontent.com/AllGray/PocketDesk/master/files/.xinitrc
