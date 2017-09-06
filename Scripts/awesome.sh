7a69446f82a0bb566914655fe534200008f8323b
AllGray <mathiasthur@gmail.com>
2017-09-06 13:28:06 +0200

    Create awesome.sh

---
+++ Scripts/awesome.sh
@@ -0,0 +1,9 @@
+# Configure Awesome
+touch  ~/.config/awesome/rc.lua  
+wget -O ~/.config/awesome/rc.lua https://raw.githubusercontent.com/AllGray/PocketDesk/master/rc.lua
+cp /usr/share/pocket-home/config.json ~/.pocket-home/config.json
+sudo rm ~/.pocket-home/.passwd/passwd
+
+sed -i -e 's/NoDisplay=true/NoDisplay=false/g'  /usr/share/xsessions/awesome.desktop
+
+Sync
