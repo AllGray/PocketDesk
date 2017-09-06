5b0766fb9235d3ff232e93308dc5c43cb9e25b37
AllGray <mathiasthur@gmail.com>
2017-09-06 13:27:27 +0200

    Create pocketHOME.sh

---
+++ Scripts/pocketHOME.sh
@@ -0,0 +1,7 @@
+# PocketHOME
+echo "deb https://o-marshmallow.github.io/PocketCHIP-pocket-home/archive/ jessie main" | sudo tee /etc/apt/sources.list.d/marshmallow-pocket-chip-home.list
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 584F7F9F
+echo -e "Package: pocket-home\nPin: version *\nPin-Priority: 1050" | sudo tee /etc/apt/preferences.d/unpin-pocket-home.pref
+apt-get update
+apt-get install pocket-home
+pocket-home
