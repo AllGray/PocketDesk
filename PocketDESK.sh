aea6a419b9ec32ba26aa293527e3fe9e12e4fc94
AllGray <mathiasthur@gmail.com>
2017-09-06 14:49:35 +0200

    Update PocketDESK.sh

--- PocketDESK.sh
+++ PocketDESK.sh
@@ -12,59 +12,28 @@ reset
 # install dependencies
 ./PocketDesk/Scripts/dependencies.sh
 
-# Sync
-sync
-
 # Install PocketHome
 ./PocketDesk/Scripts/pockethome.sh
 
-# Sync
-sync
-
-# Run PocketHome
-timeout 10 pocket-home
-
-# Sync
-sync
-
 # Configure Awesome
-./PocketDesk/Scripts/awesome.sh
-
-# Sync
-sync
+# Testing without this part!
+# ./PocketDesk/Scripts/awesome.sh
 
 # Working Touchscreen
 ./PocketDesk/Scripts/touchscreen.sh
 
-# Sync
-sync
-
 # Working Keyboard
 ./PocketDesk/Scripts/keyboard.sh
-
-# Sync
-sync
 
 # Get a battery indicator
 ./PocketDesk/Scripts/battery.sh
 
-# Sync
-sync
-
 # Nicely named sessions
 ./PocketDesk/Scripts/sessions.sh
 
-# Sync
-sync
-
 # Logout button for PocketHome
-./PocketDesk/Scripts/logout.sh
-
-# Sync
-sync
+# Not needed anymore! 
+#./PocketDesk/Scripts/logout.sh
 
 # Wallpaper Change
 ./PocketDesk/Scripts/wallpaper.sh
-
-# Sync
-sync
