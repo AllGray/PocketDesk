f5d3868f0024ba1fa436a13f5b2a748af2b8db5e
AllGray <mathiasthur@gmail.com>
2017-09-06 13:41:49 +0200

    Create PocketDESK.sh

---
+++ PocketDESK.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if user is root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Clear the screen
+reset
+
+# install dependencies
+./PocketDesk/Scripts/dependencies.sh
+
+# Install PocketHome
+./PocketDesk/Scripts/pockethome.sh
+
+# Configure Awesome
+./PocketDesk/Scripts/awesome.sh
+
+# Working Touchscreen
+./PocketDesk/Scripts/touchscreen.sh
+
+# Working Keyboard
+./PocketDesk/Scripts/keyboard.sh
+
+# Get a battery indicator
+./PocketDesk/Scripts/battery.sh
+
+# Nicely named sessions
+./PocketDesk/Scripts/sessions.sh
+
+# Logout button for PocketHome
+./PocketDesk/Scripts/logout.sh
+
+# Wallpaper Change
+./PocketDesk/Scripts/wallpaper.sh
