cd491a1f796ace76e95a222cfc28688c4d3778e8
AllGray <mathiasthur@gmail.com>
2017-09-08 01:25:35 +0200

    Update pockethome.sh

--- Scripts/pockethome.sh
+++ Scripts/pockethome.sh
@@ -1,4 +1,5 @@
 # PocketHOME
+read -r hostname < /etc/hostname
 echo "deb https://o-marshmallow.github.io/PocketCHIP-pocket-home/archive/ jessie main" | sudo tee /etc/apt/sources.list.d/marshmallow-pocket-chip-home.list
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 584F7F9F
 echo -e "Package: pocket-home\nPin: version *\nPin-Priority: 1050" | sudo tee /etc/apt/preferences.d/unpin-pocket-home.pref
@@ -6,4 +7,4 @@ apt-get update
 apt-get install pocket-home
 mkdir /home/chip/.pocket-home/
 wget -O /home/chip/.pocket-home/config.json https://raw.githubusercontent.com/AllGray/PocketDesk/master/files/config.json
-sudo chown -R chip: .pocket-home/
+sudo chown -R $hostname: .pocket-home/
