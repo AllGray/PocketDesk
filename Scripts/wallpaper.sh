8ac54e17a36adf9a50d7f868b5caaf8e039951eb
AllGray <mathiasthur@gmail.com>
2017-09-07 22:21:32 +0200

    Update wallpaper.sh

--- Scripts/wallpaper.sh
+++ Scripts/wallpaper.sh
@@ -1,4 +1,4 @@
 # Wallpaper
-wget -O /home/chip/.pocket-home/PocketDESK_wall.png https://raw.githubusercontent.com/AllGray/PocketDesk/master/images/PocketDESK_wall.png
+wget -O /home/chip/.pocket-home/PocketDESK_wall_gray_480x272.png https://raw.githubusercontent.com/AllGray/PocketDesk/master/images/PocketDESK_wall_gray_480x272.png
 # Not needed anymore
 # sed -i -e 's#"background": "4D4D4D",#"background": "/home/chip/.pocket-home/PocketDESK_wall.png",#g'  ~/.pocket-home/config.json
