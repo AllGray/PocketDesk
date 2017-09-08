555cdbbe29461cc25ed7400e3e50f1b93be214ee
AllGray <mathiasthur@gmail.com>
2017-09-08 02:52:26 +0200

    Create touchscreenlite.sh

---
+++ Scriptslite/touchscreenlite.sh
@@ -0,0 +1,4 @@
+# Touchscreen
+xinput_calibrator
+mv /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
+wget -O /etc/X11/xorg.conf https://raw.githubusercontent.com/AllGray/PocketDesk/master/files/xorg.conf
