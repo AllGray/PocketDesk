05fd3600834619c6bdc509fe76e7c42f3bb7f879
Christian Cosas <32409552+ceemonkey@users.noreply.github.com>
2019-01-18 00:21:06 -0500

    Update dependencies.sh
    Changed repository from defunct nextthing.co URL to chip.jfpossibilities.com

--- Scripts/dependencies.sh
+++ Scripts/dependencies.sh
@@ -3,6 +3,6 @@
 # install dependencies
 apt-get -y update
 apt-get -y install software-properties-common 
-add-apt-repository http://opensource.nextthing.co/chip/debian/pocketchip
+add-apt-repository http://chip.jfpossibilities.com/chip/debian/pocketchip
 apt-get -y update
 apt-get -y install git xinput-calibrator xfce4-genmon-plugin awesome apt-transport-https ahoy chip-sunvox pcmanfm pocketchip-batt pocketchip-configs pocketchip-list pocketchip-load pocketchip-localdoc pocketchip-onboard pocketchip-update pocket-home pocket-wm surf vala-terminal
